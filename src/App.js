7aebbca170ac51cbc2f5e50bd3b50fd9d232bb21
Henrique <tech@newlivseguros.com>
2025-02-01 18:06:09 -0300

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import DashboardGestor from './components/DashboardGestor';
 import DashboardCorretor from './components/DashboardCorretor';
